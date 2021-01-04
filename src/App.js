6cc4a7e817ba12a7c3cdba665cd0bd50ce3261db
Daniel <G00359991@gmit.ie>
2021-01-04 15:16:06 +0000

    First part complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'; // pulls in code from react to be used in App.js and allow for the html to be outputted into the react app
 import './App.css';
+import { Gamecontent } from './Components/Gamecontent'; // pulls in code from the Gamecontent.js component to be used in App.js
+import { Gamefooter } from './Components/Gamefooter'; // pulls in code from the Gamefooter.js component to be used in App.js
+import { Gameheader } from './Components/Gameheader'; // pulls in code from the Gameheader.js component to be used in App.js
+import 'bootstrap/dist/css/bootstrap.min.css'; // pulls down from bootstrap for added css styling in the app
+import { Navbar, Nav } from 'react-bootstrap'; // pulls the navbar css from online.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; // pulls from react-router-dom that was installed to add to the app's navbar and links
+
+
+class App extends React.Component { // extends from and uses the code from react
+
+
+  render() { 
+    return ( /* outputs the navbar and nav links to the screen */
+      <Router>
+      <div className="App">
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/mycreate">Create</Nav.Link>
+            <Nav.Link href="/myread">Read</Nav.Link>
+          </Nav>
+          </Navbar>
+          <Switch>
+            <Route path='/' component={Gamecontent} exact></Route> 
+            <Route path='/mycreate' component={Gameheader} exact></Route>
+            <Route path='/myread' component={Gamefooter} exact></Route>
+          </Switch>
+      </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+export default App; // allows the code to run and show the app made on a browser window.
